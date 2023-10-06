babafadffd148099cde86b510e429ceb209f7b80
Fransisco-BSI <145652338+Fransisco-BSI@users.noreply.github.com>
2023-10-06 11:40:00 +0700

    Pustaka-Booking
    Web Programming

---
+++ application/views/homepage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>test</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free Website Template" name="keywords">
+    <meta content="Free Website Template" name="description">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <div class="sidebar-text d-flex flex-column h-100 justify-content-center text-center">
+                <h1 class="font-weight-bold">Fransisco</h1>
+                <p class="mb-4">
+                    Nama saya Fransisco <br> saya tinggal di daerah Kemang<br>
+                    saat ini saya sedang berkuliah di Universitas BSI di Bogor A (Merdeka)
+                </p>
+            </div>
+        </div>
+</body>
+
+</html>
